8ead81c9ad608dc5dcefed5151c90615849a66a2
Manikandan Sundararajan <mani@hey.com>
2022-06-14 13:54:53 -0400

    send only the required data to the client via prisma select

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,15 +1,15 @@
 import { Link, useLoaderData } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { getPosts } from "~/models/post.server";
+import { getPostListings, getPosts } from "~/models/post.server";
 
 // LoaderData is a new type of a promise that contains the return type of the getPosts function
 type LoaderData = {
-  posts: Awaited<ReturnType<typeof getPosts>>;
+  posts: Awaited<ReturnType<typeof getPostListings>>;
 };
 
 export const loader: LoaderFunction = async () => {
-  const posts = await getPosts();
+  const posts = await getPostListings();
   return json({ posts });
 };
 
