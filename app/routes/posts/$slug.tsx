9133f6cd9b3f77dc7cbb8fe589a0aa82caa79838
Manikandan Sundararajan <mani@hey.com>
2022-06-18 00:46:02 -0400

    set html and use invariant conditions

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,24 +1,32 @@
+import { marked } from "marked";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { getPost } from "~/models/post.server";
+import invariant from "tiny-invariant";
 
 type LoaderData = {
-  post: Awaited<ReturnType<typeof getPost>>;
+  title: string;
+  html: string;
 };
 
+// Loader runs on the server so we don't do any unnecessary processing on the client
 export const loader: LoaderFunction = async ({ params }) => {
   const { slug } = params;
+  invariant(slug, "slug is required"); // verify that the slug is defined
   const post = await getPost(slug);
-  return json({ post });
+
+  invariant(post, `post not found: ${slug}`); // verify that the post was found
+  const html = marked(post.markdown);
+  return json<LoaderData>({ title: post.title, html });
 };
 
 export default function PostRoute() {
-  const { post } = useLoaderData() as LoaderData;
+  const { title, html } = useLoaderData() as LoaderData;
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">{post?.title}</h1>
-      <div>{post?.markdown}</div>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
